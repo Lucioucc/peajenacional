7e163be0d2d7a724e7e8eb0158d4147eb4dc05fc
Lucioucc <2500097@ucc.edu.ar>
2025-06-08 17:50:31 -0400

    Delete HTML-LAB directory

--- HTML-LAB/java/login.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('entrarBtn').addEventListener('click', function() {
-  const provincia = document.getElementById('provinciaSelect').value;
-  if (!provincia) {
-    alert('Por favor, seleccione una provincia.');
-    return;
-  }
-  window.location.href = 'registro.html?provincia=' + encodeURIComponent(provincia);
-});
